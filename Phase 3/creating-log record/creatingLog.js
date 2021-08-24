c81ba4d9b54bf9c5190cce1ef95b657529001271
BIsrat2017 <bisratmendefera@gmail.com>
2021-08-23 20:30:23 -0700

    Phase-3 CreatingLog

--- Phase 3/creating-log record/creatingLog.js
+++ Phase 3/creating-log record/creatingLog.js
@@ -4,16 +4,19 @@ const { debug } = require("console");
 
 function addRecord(){
     let records=[];
+    debugger;
     let fName=input.question("Whats is Your First Name: ")
     let lName=input.question("Whats is Your Last Name: ")
-    debug;
+    debugger;
     let gender=input.question("Whats is Your Gender: ")
     let email=input.questionEMail("Whats is Your email: ")
+    debugger;
     let date=Date().toString()
-
     let detail = {fName:fName,lName:lName,gender:gender,email:email,date:date}
+    debugger;
     records.push(detail)
     let data=JSON.parse(fs.readFileSync("detail.json").toString())
+    debugger;
     data.push(detail);
     // data.push("\n")
     fs.writeFileSync("detail.json",JSON.stringify(data))
