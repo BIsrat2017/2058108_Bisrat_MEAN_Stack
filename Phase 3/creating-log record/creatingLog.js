9adf0801ba9a1dd19d71933c50cab52837b1ed62
BIsrat2017 <bisratmendefera@gmail.com>
2021-08-24 12:01:08 -0700

    done section 2

--- Phase 3/creating-log record/creatingLog.js
+++ Phase 3/creating-log record/creatingLog.js
@@ -11,8 +11,13 @@ function addRecord(){
     let gender=input.question("Whats is Your Gender: ")
     let email=input.questionEMail("Whats is Your email: ")
     debugger;
-    let date=Date().toString()
-    let detail = {fName:fName,lName:lName,gender:gender,email:email,date:date}
+    let date= new Date()
+    let fullDate = date.getDay()+"-"+date.getMonth()+"-"+date.getFullYear();
+    let Ntime=date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+    
+    fullDate=fullDate+"  "+Ntime
+
+    let detail = {fName:fName,lName:lName,gender:gender,email:email,date:fullDate}
     debugger;
     records.push(detail)
     let data=JSON.parse(fs.readFileSync("detail.json").toString())
